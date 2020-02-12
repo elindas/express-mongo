73101768dee420dc2e7d7a772840e8b416e06f29
Elinda S <elinda.sinta@gmail.com>
2020-02-12 08:34:29 +0700

    add update

--- routes/students/controller.js
+++ routes/students/controller.js
@@ -3,7 +3,10 @@ const { Students } = require("../../models");
 module.exports = {
     getAll: async (req, res) => {
         try {
-            const result = await Students.find({}).populate("user");
+            const result = await Students.find({}).populate(
+                "user",
+                "firstName email age"
+            );
 
             res.status(200).send({
                 message: "Show datas students",
@@ -17,7 +20,7 @@ module.exports = {
         try {
             const data = req.body;
 
-            const result = await Students.create(data)
+            const result = await Students.create(data);
 
             res.status(200).send({
                 message: "New data user is successfully added",
@@ -31,12 +34,35 @@ module.exports = {
         try {
             const result = await Students.findOne({
                 name: req.params.name
-            }).populate('user')
+            }).populate("user", "firstName email age");
 
             res.status(200).send({ message: "Show by name", data: result });
         } catch (error) {
             console.log(error);
         }
     },
-};
+    deleteByName: async (req, res) => {
+        try {
+            const deleted = await Students.deleteOne({ name: req.params.name });
 
+            res.status(200).send({ message: "Delete by name", data: deleted });
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    updateById: async (req, res) => {
+        try {
+            let data = req.body;
+
+            let update = await Students.findByIdAndUpdate(req.params.id, {score: req.body.score});
+             await update.save();
+            // const data= req.body
+            res.status(200).send({
+                message: "Show data updated",
+                data: data
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
--- routes/students/index.js
+++ routes/students/index.js
@@ -7,6 +7,11 @@ router.get("/", require("./controller").getAll);
 router.get("/:name", require("./controller").getByName);
 router.post("/", require("./controller").postData);
 
+router.delete("/:name", require("./controller").deleteByName);
+router.put("/:id", require("./controller").updateById);
+
+
+
 
 
 module.exports = router;
